a39e9e5465186fdb5dd184b7f42c24502241a714
Abdul Sami <abdulsamikhalife@gmail.com>
2025-08-09 18:31:52 +0530

    Update useLenis.js

--- src/services/useLenis.js
+++ src/services/useLenis.js
@@ -4,6 +4,13 @@ import Lenis from "lenis";
 
 const useLenis = () => {
   useEffect(() => {
+    // Only initialize Lenis on desktop devices
+    const isMobile = window.innerWidth < 768 || /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    
+    if (isMobile) {
+      return; // Don't initialize Lenis on mobile
+    }
+
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // easeOutExpo
